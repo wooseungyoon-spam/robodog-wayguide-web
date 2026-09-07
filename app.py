79a78bc1fe4e9fd4322de90ddf5f337cd97650e5
wooseungyoon <woo.seungyoon2012@gmail.com>
2026-09-07 16:26:58 +0900

    fix: disable static/template caching in Flask and add Cache-Control no-store headers

--- app.py
+++ app.py
@@ -24,6 +24,15 @@ logging.basicConfig(
 logger = logging.getLogger("RoboDogHub")
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+@app.after_request
+def add_cache_control_headers(response):
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
 
 @app.before_request
 def log_request_info():
